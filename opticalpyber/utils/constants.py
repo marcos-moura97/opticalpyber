5fe4572783917106a0e52996dbb1ae83bf8948b6
Marcos Moura <gabbyru2@gmail.com>
2022-04-21 08:11:49 -0300

    feat: add vacuum permeability

--- opticalpyber/utils/constants.py
+++ opticalpyber/utils/constants.py
@@ -7,4 +7,5 @@ import math
 
 PI         = math.pi
 C_0        = 3E8           #velocidade da luz no vácuo
-MI_0       = 4*PI*10**-7   #permeabilidade no vácuo
+MU_0       = 4*PI*10**-7   #permeabilidade no vácuo
+EPSILON_0  = (MU_0*C_0**2)**-1
